328f9d9250d5a05fcbabd49e957ccf9aa8f40c1c
AnthonyPillow <anthonypillow18@gmail.com>
2020-02-08 12:19:14 -0800

    Added new function to store new ingredients in local storage and worked more on the appendNewIngredient function so it appends the new ingredient at the end of the ingredient list.

--- jscript-0208.js
+++ jscript-0208.js
@@ -2,15 +2,18 @@ const searchBtn = $("#")
 const clearIngredientsBtn = $("#")
 const addIngredientBtn = $("#add-ingredient")
 const ingredientList = $("#ingredient-list")
-const newIngredientInput = $("#new-ingredient-input")
+const newIngredientInput = document.querySelector("#new-ingredient-input")
 
+var ingredientTextInput = $("<input>").attr("type", "text")
+var submitNewIngredientBtn = $("<button>").text("Add New Ingredient")
 // Adding on click listeners to buttons
 
 // Search button listener
-$(searchBtn).on("click", function(){
-	preventDefault()
-	apiSearch()
-})
+
+// $(searchBtn).on("click", function(){
+// 	preventDefault()
+// 	apiSearch()
+// })
 
 // Clear all checked ingredients button
 $(clearIngredientsBtn).on("click",function(){
@@ -24,16 +27,42 @@ $(addIngredientBtn).on("click", function(e){
 	addNewIngredient()
 })
 
+// Add button to submit a new ingredient
+$(submitNewIngredientBtn).on("click", function(){
+	appendNewIngredient()
+	storeNewIngredient()
+})
+
+// On submit of a form
+// $("#target-form").submit(function(e){
+// 	console.log("g")
+// })
+
 // Adding Functions
 
 // Clears all checked ingredients
 function clearCheckedIngredients(){
-
+	
 }
 
 // Appends a text input and adds a new ingredient from that search
 function addNewIngredient(){
 	$(newIngredientInput).empty()
-	$("<input>").attr("type", "text")
-	.appendTo(newIngredientInput)
+
+	$(ingredientTextInput).appendTo(newIngredientInput)
+
+	$(submitNewIngredientBtn).appendTo(newIngredientInput)
+}
+
+
+function appendNewIngredient(){
+	var ingredient = $("<li>").text(ingredientTextInput.val())
+
+	$(ingredient).appendTo(ingredientList)
+}
+
+function storeNewIngredient(){
+	var ingredient = ingredientTextInput.val()
+
+	console.log(ingredient)
 }
