3819bf556ee0b1e66241d56e936c2946a9f7f3cc
G Val <13578427+gusvalenzuela@users.noreply.github.com>
2020-02-08 10:24:26 -0800

    new ajax call

--- jscript-0208.js
+++ jscript-0208.js
@@ -1,15 +1,15 @@
-var unirest = require("unirest");
+var searchPara = `chinese`
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://tasty.p.rapidapi.com/recipes/list?&q="+searchPara+"&from=0&sizes=20",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "tasty.p.rapidapi.com",
+		"x-rapidapi-key": "859d702838msh5b93a71ca2adb7dp16e714jsnd5e12694a313"
+	}
+}
 
-var req = unirest("GET", "https://tasty.p.rapidapi.com/tags/list");
-
-req.headers({
-	"x-rapidapi-host": "tasty.p.rapidapi.com",
-	"x-rapidapi-key": "859d702838msh5b93a71ca2adb7dp16e714jsnd5e12694a313"
-});
-
-
-req.end(function (res) {
-	if (res.error) throw new Error(res.error);
-
-	console.log(res.body);
+$.ajax(settings).then(function (response) {
+	console.log(response);
 });
