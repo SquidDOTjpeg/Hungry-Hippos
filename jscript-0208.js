9a17f10b57fbe3c83f593c3cb64e6d6cc973bfdf
G Val <13578427+gusvalenzuela@users.noreply.github.com>
2020-02-08 17:28:39 -0800

    minor changes

--- jscript-0208.js
+++ jscript-0208.js
@@ -1,21 +1,18 @@
-var searchPara, userRatingScore, sampleIngredient, mealURL, mealImg;
-var recipeArray = [];
 const searchBarInput = $(`#search-bar-input`)
 const searchBarDiv = $(`#search-bar-div`)
-const targetForm = $(`#target-form`)
+const searchForm = $(`#search-form`)
+var searchPara, topMeals, userRatingScore, sampleIngredient, mealURL, mealImg;
+var recipeArray = [];
+// var recipestoPull = 20
 
-targetForm.submit(function(e){
-    e.preventDefault()
-    el = e.target
-    console.log(el)
-    console.log(searchBarInput[0].value)
-    searchPara = searchBarInput[0].value
+searchForm.submit(function(e){
+	e.preventDefault()
+	searchPara = searchBarInput[0].value 	// places value of search bar input into variable, on submit (enter)
+
     runRecipeAjax()
 })
 
 function runRecipeAjax(){
-    console.log(`======= runAjax() START =======`)
-
 
     var settings = {
         "async": true,
@@ -29,47 +26,58 @@ function runRecipeAjax(){
     }
 
     $.ajax(settings).then(function (response) {
-        console.log(`======= response received START =======`)
-        recipeArray = []
-        console.log(response);
-    
+        console.log(`======= AJAX response (received) START =======`)
+		console.log(`response obj: `)
+		console.log(response);
+		
+		recipeArray = []		// clearing the array
+	
+		// loop through array of responses and grab what you need (put in obj)
         for(j=0;j<response.results.length;j++){
             var newObj = {};
-            newObj.name = response.results[j].name
+			var ingredientResponse
+			
+            newObj.name = response.results[j].name						// name of recipe
             newObj.video_url = response.results[j].video_url
             newObj.thumbnail_url = response.results[j].thumbnail_url
-            newObj.ingredientsArray = []
-            var ingredientResponse
+            newObj.ingredientsArray = []								// making a clear array to house ingredients looped later
 
             if(response.results[j].sections !== undefined){
                 newObj.userRating = response.results[j].user_ratings.score
                 ingredientResponse = response.results[j].sections[0].components
             }
-    
+			
+			// loop through each ingredients and push to array for easy access 
             for(var i=0;i<ingredientResponse.length;i++){
+				// some indices in the "ingredients" response array are "n/a", this just ignores them for pushing
                 if(ingredientResponse[i].raw_text !== 'n/a'){
                     newObj.ingredientsArray.push(ingredientResponse[i].raw_text)
                 } 
             }
-            recipeArray.push(newObj)
-        }
-        console.log(recipeArray)
-        showResults()
+            recipeArray.push(newObj)			// push whole new obj into the recipe array
+		}
 
-        console.log(`======= response received END =======`)
+        showResults()
+		
+		// console.log(`The recipe array is:`)
+		// console.log(recipeArray)	
+
+        console.log(`======= AJAX response END =======`)
     });
-    console.log(`======= runAjax() END =======`)
 }
 function showResults(){
-    var ul = $(`<ul>`)
-    for(i=0;i<5;i++){
+	var ul = $(`<ul>`)
+	var itemsToShow = 5			// amount of meals to show, to choose from
+	
+    for(i=0;i<itemsToShow;i++){
         var result = $(`<li>`).attr(`class`, ``).attr(`id`, `list-item`+i)
         result.append($(`<img>`).attr(`src`,recipeArray[i].thumbnail_url).attr(`style`,`width: 6em`))
         result.append($(`<button>`).attr(`id`,`button-`+i).attr(`type`,`button`).attr(`class`,`meal-buttons`).text(recipeArray[i].name))
         ul.append(result)
     }
 
-    searchBarDiv.append(ul)
+	searchBarDiv.append(ul)
+	searchBarDiv.append($(`<button>`).attr(`type`, `button`).attr(`id`,``).text(`Next 5`))
     
     $(`.meal-buttons`).on(`click`,function(e){
         var el = e.target
