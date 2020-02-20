5366a3bd29cf7cacfba42ce91994c2e3e37ad535
AnthonyPillow <anthonypillow18@gmail.com>
2020-02-20 11:14:42 -0800

    Added a seperate index file for the mapbox map and another button to the main index to navigate there

--- mapbox-js-0219.js
+++ mapbox-js-0219.js
@@ -1,52 +1,63 @@
-  mapboxgl.accessToken = 'pk.eyJ1Ijoic3F1aWRkb3RqcGVnIiwiYSI6ImNrNmxsMG43dTBjczQzanBkODhjaWd2OWMifQ.HY9YBWTOksKHlIglaCkVlA';
-  var map = new mapboxgl.Map({
+mapboxgl.accessToken = 'pk.eyJ1Ijoic3F1aWRkb3RqcGVnIiwiYSI6ImNrNmxsMG43dTBjczQzanBkODhjaWd2OWMifQ.HY9YBWTOksKHlIglaCkVlA';
+var map = new mapboxgl.Map({
     container: 'map', // Container ID
     style: 'mapbox://styles/mapbox/streets-v11', // Map style to use
     center: [-121.4944, 38.5816], // Starting position [lng, lat]
     zoom: 12, // Starting zoom level
-  });
+});
 
-    
 
-  var geocoder = new MapboxGeocoder({ // Initialize the geocoder
+
+var geocoder = new MapboxGeocoder({ // Initialize the geocoder
     accessToken: mapboxgl.accessToken, // Set the access token
     mapboxgl: mapboxgl, // Set the mapbox-gl instance
     marker: false, // Do not use the default marker style
     placeholder: 'Search for grocery stores in Sacramento', // Placeholder text for the search bar
-    proximity: {
-      longitude: -121.4944,
-      latitude: 38.5816
-    } // Coordinates of Sacramento
-  });
+    // bbox: [-121.49445, 38.84214, -121.30937, 38.89838,],
+    // proximity: {
+    //     longitude: -121.4944,
+    //     latitude: 38.5816
+    // } // Coordinates of Sacramento
+});
 
-    // Add the geocoder to the map
-  map.addControl(geocoder);
+// Add geolocate control to the map.
+map.addControl(
+    new mapboxgl.GeolocateControl({
+        positionOptions: {
+            enableHighAccuracy: true
+        },
+        trackUserLocation: true
+    })
+);
 
-  // After the map style has loaded on the page,
+// Add the geocoder to the map
+map.addControl(geocoder);
+
+// After the map style has loaded on the page,
 // add a source layer and default styling for a single point
-map.on('load', function() {
+map.on('load', function () {
     map.addSource('single-point', {
-      type: 'geojson',
-      data: {
-        type: 'FeatureCollection',
-        features: []
-      }
+        type: 'geojson',
+        data: {
+            type: 'FeatureCollection',
+            features: []
+        }
     });
-  
+
     map.addLayer({
-      id: 'point',
-      source: 'single-point',
-      type: 'circle',
-      paint: {
-        'circle-radius': 10,
-        'circle-color': '#448ee4'
-      }
+        id: 'point',
+        source: 'single-point',
+        type: 'circle',
+        paint: {
+            'circle-radius': 10,
+            'circle-color': '#448ee4'
+        }
     });
-  
+
     // Listen for the `result` event from the Geocoder
     // `result` event is triggered when a user makes a selection
     //  Add a marker at the result's coordinates
-    geocoder.on('result', function(e) {
-      map.getSource('single-point').setData(e.result.geometry);
+    geocoder.on('result', function (e) {
+        map.getSource('single-point').setData(e.result.geometry);
     });
-  });
+});
